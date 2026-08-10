758e94f0efe01e1f3f9432e8a4b87c4373001b6b
Alex Uparkoti <alexuparkoti@gmail.com>
2026-08-10 12:15:56 +0545

    for loop in python

--- condition.py
+++ condition.py
@@ -1,7 +1,5 @@
 a=int(input("enter your age:"))
 if a>=18:
     print("you are eligible to vote")
-elif a<18:
-    print("you are not eligible to vote")
 else:
     print("you are not eligible to vote")
