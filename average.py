e776782dabe875b04c630163fc349b939c031ef6
Alex Uparkoti <alexuparkoti@gmail.com>
2026-08-20 15:32:22 +0545

    asking user to get number and find the average number

---
+++ average.py
@@ -0,0 +1,8 @@
+def calculate_average(a, b, c):
+    avg=(a + b + c) / 3
+    print(avg)
+
+
+calculate_average(1, 2, 3)    
+calculate_average(1, 2, 3)   
+calculate_average(1, 2, 3)   
---
+++ average_2.py
@@ -0,0 +1,9 @@
+a=int(input("enter first number:"))
+b=int(input("enter second number:"))
+c=int(input("enter third number:"))
+
+def average(a, b, c):
+    avg=(a + b + c) / 3
+    print(avg)
+
+average(a, b, c)
